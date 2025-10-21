5b480e6e5c7f8376a819d808d52f3c3fef50da86
pendko1 <marvjeff02@gmail.com>
2025-10-21 17:02:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 # 1 import library
 from sklearn.linear_model import LinearRegression
+import pandas as pd
 import matplotlib.pyplot as plt
+import streamlit as st
 
 # 2 menyiapkan data
-X = [[1], [2], [3], [4]] # two dimensional list / array
-y = [101, 102, 103, 104] # one dimensional list / aray
+X = [[1], [2], [3], [4]] #tipe data list 2 dimensi
+y = [101, 102, 103, 104] #tipe data list 1 dimensi
 
-# 3 instansiasi objek linear regression
+# 3 instansiasi objek
 model = LinearRegression()
 
-# 4 melatih model linear regression
+st.title("Prediksi Gaji")
+
+# 4 training
 model.fit(X, y)
+input_user = st.number_input("Masukan value :")
 
-# 5 print hasil prediksi
-print(model.predict([[20]]))
+prediction = model.predict([[input_user]])
 
-# 5 Visualisasi dengan library Matplotlib
-plt.scatter(X, y)
-plt.plot(X, y)
-plt.scatter(20, model.predict([[20]]))
-plt.show()
+# 6 visualisasi
+fig, ax = plt.subplots()
+
+prediksi_y = model.predict(X)
+ax.scatter(X, y)
+ax.plot(X, prediksi_y)
+ax.scatter([input_user], [prediction])
+
+st.pyplot(fig)
+st.metric(label="Gaji", value=prediction)
