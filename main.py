bfe954fa0739821fe69cdb81cedb18b5567208d1
pendko1 <marvjeff02@gmail.com>
2025-12-10 13:45:54 +0700

    Refactor salary prediction with Streamlit and CSV data
    Updated the salary prediction model to use data from a CSV file and added Streamlit components for user input and visualization.

--- main.py
+++ main.py
@@ -1,31 +1,44 @@
-# 1 import library
-from sklearn.linear_model import LinearRegression
 import pandas as pd
-import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 import streamlit as st
+import matplotlib.pyplot as plt
 
-# 2 menyiapkan data
-X = [[1], [2], [3], [4]] #tipe data list 2 dimensi
-y = [101, 102, 103, 104] #tipe data list 1 dimensi
+df = pd.read_csv('indonesian_salary_by_region.csv')
 
-# 3 instansiasi objek
+st.title("Prediksi Gaji UMP/UMK Indonesia")
+st.markdown("---")
+
+region = df['REGION'].unique()
+
+input_region = st.selectbox("Pilih Provinsi", options= region)
+
+df_selected = df[df['REGION'] == input_region].copy()
+
+X = df_selected[['YEAR']] # independent 
+y = df_selected['SALARY'] # dependent
+
 model = LinearRegression()
-
-st.title("Prediksi Gaji")
-
-# 4 training
 model.fit(X, y)
-input_user = st.number_input("Masukan value :")
 
-prediction = model.predict([[input_user]])
+min_year = df_selected['YEAR'].min()
+max_year = df_selected['YEAR'].max()
+input_year = st.number_input("Masukan Tahun: ", min_value = min_year,  value = max_year+1)
 
-# 6 visualisasi
-fig, ax = plt.subplots()
+prediction = model.predict([[input_year]])
 
-prediksi_y = model.predict(X)
-ax.scatter(X, y)
-ax.plot(X, prediksi_y)
-ax.scatter([input_user], [prediction])
+st.metric( label=f'Prediksi Gaji di Provinsi {input_region} di Tahun {input_year}',value= prediction[0])
+
+st.subheader("Graph hooh")
+
+fig, ax = plt.subsplot()
+ax.scatter (x, y, label=f 'diem aja bersantai {input_region} 1997-2025', color = 'blue')
+ax.plot(X, model.predict(x), color ='red', label="gataulah")
+
+ax.scatter([input_year], [prediction], color='green', label=f 'pred thn 26{input_year}')
+
+ax.set_xlabel("Tahun")
+ax.set_ylabel("Gaji")
+ax.set_title("tulis sendiri males aku")
+ax.legend()
 
 st.pyplot(fig)
-st.metric(label="Gaji", value=prediction)
